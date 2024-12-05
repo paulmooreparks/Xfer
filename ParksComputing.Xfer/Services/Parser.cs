8ae0b51a5c064e0d49870c9c559319b8333c7b40
Paul Parks <paulp@agilebridge.co.za>
2024-12-05 17:04:56 +0800

    Increment language/parser version.

--- ParksComputing.Xfer/Services/Parser.cs
+++ ParksComputing.Xfer/Services/Parser.cs
@@ -12,7 +12,7 @@ namespace ParksComputing.Xfer.Services;
 settle on a solid grammar, I'll redo the parser or use some kind of tool to generate it. */
 
 public class Parser {
-    public static readonly string Version = "0.7.0";
+    public static readonly string Version = "0.7.2";
 
     public Parser() : this(Encoding.UTF8) { }
 
