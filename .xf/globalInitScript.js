d030df52808231a0d04eb6ae62a6f2ca3c8ad521
Paul Parks <paulp@agilebridge.co.za>
2025-02-28 06:26:01 +0800

    Added a sample .xf configuration folder

---
+++ .xf/globalInitScript.js
@@ -0,0 +1,16 @@
+var ParksComputing_Xfer_Lang = importNamespace('ParksComputing.Xfer.Lang');
+var ParksComputing_Xfer_Lang_Elements = importNamespace('ParksComputing.Xfer.Lang.Elements');
+var XferStringElement = ParksComputing_Xfer_Lang_Elements.StringElement;
+var XferFormatting = ParksComputing_Xfer_Lang.Formatting;
+var XferElementStyle = ParksComputing_Xfer_Lang.ElementStyle;
+var XferElementStyleExplicit = ParksComputing_Xfer_Lang.ElementStyle.Explicit;
+// var stringElement = new XferStringElement("I'm a string");
+// log(stringElement.ToXfer(XferFormatting.Pretty));
+
+/* You may uncomment the following lines after you've installed the Newtonsoft.Json package */
+
+// var Newtonsoft_Json = importNamespace('Newtonsoft.Json');
+// var Newtonsoft_Json_Linq = importNamespace('Newtonsoft.Json.Linq');
+// var JsonConvert = Newtonsoft_Json.JsonConvert;
+// var JObject = Newtonsoft_Json_Linq.JObject;
+
