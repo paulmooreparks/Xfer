9cc9a3439a00c861978e20763ac4cfcc650a950a
Paul Parks <paulp@agilebridge.co.za>
2025-03-05 17:50:24 +0800

    Updated sample files.

--- .xf/globalInitScript.js
+++ .xf/globalInitScript.js
@@ -1,13 +1,12 @@
-var ParksComputing_Xfer_Lang = importNamespace('ParksComputing.Xfer.Lang');
-var ParksComputing_Xfer_Lang_Elements = importNamespace('ParksComputing.Xfer.Lang.Elements');
-var XferStringElement = ParksComputing_Xfer_Lang_Elements.StringElement;
-var XferFormatting = ParksComputing_Xfer_Lang.Formatting;
-var XferElementStyle = ParksComputing_Xfer_Lang.ElementStyle;
-var XferElementStyleExplicit = ParksComputing_Xfer_Lang.ElementStyle.Explicit;
-// var stringElement = new XferStringElement("I'm a string");
-// log(stringElement.ToXfer(XferFormatting.Pretty));
+// var ParksComputing_Xfer_Lang = importNamespace('ParksComputing.Xfer.Lang');
+// var ParksComputing_Xfer_Lang_Elements = importNamespace('ParksComputing.Xfer.Lang.Elements');
+// var XferStringElement = ParksComputing_Xfer_Lang_Elements.StringElement;
+// var XferFormatting = ParksComputing_Xfer_Lang.Formatting;
+// var XferElementStyle = ParksComputing_Xfer_Lang.ElementStyle;
+// var XferElementStyleExplicit = ParksComputing_Xfer_Lang.ElementStyle.Explicit;
 
-/* You may uncomment the following lines after you've installed the Newtonsoft.Json package */
+// var stringElement = new XferStringElement("I'm a string", 1, 0);
+// log(stringElement.ToXfer(XferFormatting.Pretty));
 
 // var Newtonsoft_Json = importNamespace('Newtonsoft.Json');
 // var Newtonsoft_Json_Linq = importNamespace('Newtonsoft.Json.Linq');
